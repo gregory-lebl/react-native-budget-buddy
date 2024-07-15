711dac1999492fb76bf4a904659c7eae724ad04e
Gregory <gregory.leblond.dev@gmail.com>
2024-07-15 15:46:50 +0200

    feat: setup

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./Home.style";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default Home;
